254427425a22258afc0c18973763d15ec034bff2
Fish <lavender.puzzle@gmail.com>
2023-09-15 01:42:20 +0800

    [update] single

---
+++ src/main/java/org/example/creator/single/Client.java
@@ -0,0 +1,19 @@
+package org.example.creator.single;
+
+import org.example.Main;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Client {
+    private final static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                Single single = Single.getSingle();
+                logger.info("{} ===> {}", single.getClass().getName() + "@" + Integer.toHexString(single.hashCode()), single.echo(String.valueOf(finalI)));
+            }).start();
+        }
+    }
+}
---
+++ src/main/java/org/example/creator/single/Single.java
@@ -0,0 +1,23 @@
+package org.example.creator.single;
+
+public class Single {
+    private Single() {
+    }
+
+    private static volatile Single single;
+
+    public static Single getSingle() {
+        if (single == null) {
+            synchronized (Single.class) {
+                if (single == null) {
+                    single = new Single();
+                }
+            }
+        }
+        return single;
+    }
+
+    public String echo(String s) {
+        return s;
+    }
+}
