38018e0c7fa3027912b24278b2b527dcb5417624
Fish <lavender.puzzle@gmail.com>
2023-09-14 23:59:00 +0800

    [update] main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private final static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
         for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+            new Thread(() -> logger.info("Hello and welcome!")).start();
         }
     }
 }
